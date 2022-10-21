d4243084103dbc0c7178f92b81f800916e879970
Madhu-Gandham <116324121+Madhu-Gandham@users.noreply.github.com>
2022-10-21 16:54:25 +0530

    Add files via upload

---
+++ Day 17_6/Hib/src/test/java/g1/Hib/AppTest.java
@@ -0,0 +1,12 @@
+package g1.Hib;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+    App.main(null);
+  }
+
+}
---
+++ Day 17_6/SpringMvc/src/main/java/g1/SpringMvc/controller/HomeController.java
@@ -0,0 +1,23 @@
+package g1.SpringMvc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+		return new ModelAndView("home");
+	}
+	
+	@RequestMapping(value="/narayana")
+	public ModelAndView narayana(HttpServletResponse response) throws IOException{
+		return new ModelAndView("narayana");
+	}
+}
