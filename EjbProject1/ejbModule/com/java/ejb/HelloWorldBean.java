20180187fddd7a6c6932a8c23a682e74ed603723
Madhu-Gandham <116324121+Madhu-Gandham@users.noreply.github.com>
2022-11-11 14:37:37 +0530

    Add files via upload

---
+++ EjbProject1/ejbModule/com/java/ejb/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.java.ejb;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class HelloWorldBean
+ */
+@Stateless
+@Remote(HelloWorldBeanRemote.class)
+public class HelloWorldBean implements HelloWorldBeanRemote {
+
+	
+    
+    public HelloWorldBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+	@Override
+	public String sayHello() {
+		// TODO Auto-generated method stub
+		return "Welcome to EJB Programmig...From Prasanna";
+	}
+
+}
