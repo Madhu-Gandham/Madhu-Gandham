390b3156c52e1d2c7c6d5ba0a2ea8e96cd4d67f5
Madhu-Gandham <116324121+Madhu-Gandham@users.noreply.github.com>
2022-11-14 17:45:29 +0530

    Add files via upload

---
+++ cal/JBossCalWeb/src/com/ejb/calc/CalcServlet.java
@@ -0,0 +1,65 @@
+package com.ejb.calc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class CalcServlet
+ */
+@WebServlet("/CalcServlet")
+public class CalcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		CalcBeanRemote service = null;
+
+	    // Context compEnv = (Context) new InitialContext().lookup("java:comp/env");
+
+	    // service = (HelloService)new
+	    // InitialContext().lookup("java:comp/env/ejb/HelloService");
+	    try {
+			service = (CalcBeanRemote)
+					new InitialContext().lookup("CalcBean/remote");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    PrintWriter out = response.getWriter();
+	    
+	    out.println("Sum is  " +service.sum(12, 5));
+	   out.println("Sub is  " +service.sub(12, 5));
+	  out.println("Mult is  " +service.mult(12, 5));
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
