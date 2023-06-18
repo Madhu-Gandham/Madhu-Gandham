3c06e2253381bd3a2d0a3568db8e1b45e52468f9
Madhu-Gandham <116324121+Madhu-Gandham@users.noreply.github.com>
2023-06-18 11:00:42 +0530

    Add files via upload

---
+++ youtubeProjects-master/JpaDemo/src/main/java/com/telusko/JpaDemo/Test.java
@@ -0,0 +1,12 @@
+package com.telusko.JpaDemo;
+
+public class Test {
+	public static void main(String[] args) {
+		Alien a = new Alien();
+		System.out.println(a.getAid());
+		System.out.println(a.getAname());
+		System.out.println(a.getTech());
+		
+	}
+
+}
